c64ea0c8bcf05e6da8b2acb7812bd1c2fd09d47c
nily-fri <81628912+nily-fri@users.noreply.github.com>
2021-07-06 20:08:24 +0300

    created app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <h1>Weather App</h1>
+        <Search />
+        <footer>
+          <p>
+            Coded by{" "}
+            <a
+              href="https://github.com/nily-fri?tab=repositories"
+              target="_blank"
+            >
+              Nily Friedman
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
