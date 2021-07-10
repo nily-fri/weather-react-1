90d70b5c13a2048005d19057bce8bed3b7dcf1ea
nily-fri <81628912+nily-fri@users.noreply.github.com>
2021-07-10 12:13:59 +0300

    moved everything to weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,23 +1,67 @@
 import React from "react";
 import "./Weather.css";
-import Header from "./Header.js";
-import Conditions from "./Conditions";
-import Search from "./Search.js";
 
 export default function Weather() {
   return (
     <div className="weather container ">
-      <Header city="Tel Aviv" date="Tue, Apr 6" />
-      <Search />
-      <Conditions
-        description="clear"
-        precipitation="30"
-        humidity="64"
-        feelsLike="15"
-        exactTemp="15"
-        highTemp="20"
-        lowTemp="14"
-      />
+      <h1>Tel Aviv</h1>
+      <h2>Sat, Jul 10</h2>
+      <div className="search-city">
+        <form>
+          <div>
+            <input type="search" placeholder="Location" />
+          </div>
+          <div>
+            <input
+              type="image"
+              src="https://static.thenounproject.com/png/197410-200.png"
+              alt="submit"
+              className="search-engine"
+              width="20px"
+            />
+            <input
+              type="image"
+              src="https://www.iconpacks.net/icons/2/free-location-pin-icon-2964-thumb.png"
+              alt="pin"
+              width="20px"
+              className="pin-icon"
+            />
+          </div>
+        </form>
+      </div>
+      <div className="row exact-conditions">
+        <div className="col-6">
+          <ul>
+            <li>Clear</li>
+            <li>
+              {" "}
+              Precipitation <span>30</span>%{" "}
+            </li>
+            <li>
+              Humidity: <span>30</span>%
+            </li>
+            <li>
+              Feels like <span>29</span>°
+            </li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <img
+            src="https://www.creativefabrica.com/wp-content/uploads/2020/04/21/sun-Icon-Vector-thin-line-Graphics-3920713-1-1-580x386.jpg"
+            alt="sun-icon"
+            className="big-icon"
+          />
+          <div className="today-temp">
+            <span className="exact-temp"> 30°</span>
+            <span className="degrees">
+              <a href="/">C</a> |<a href="/">F</a>
+            </span>
+            <p>
+              <strong>31°</strong> / 15°
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
